ef0438a3f2d386195fdee41baac15b0f66af9ea0
Nobile Agustín <anobile@andreani.com>
2023-03-01 14:30:16 -0300

    Etapa 2 terminada

--- src/Application/UseCase/V1/PedidoOperation/Commands/AsignarPedidoCommand.cs
+++ src/Application/UseCase/V1/PedidoOperation/Commands/AsignarPedidoCommand.cs
@@ -1,17 +1,39 @@
-﻿using Andreani.Scheme.Onboarding;
+﻿using Andreani.ARQ.Core.Interface;
+using Andreani.Scheme.Onboarding;
+using MediatR;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace OnboardingServicioNotificaciones.Application.UseCase.V1.PedidoOperation.Commands
 {
-    public class AsignarPedidoCommand
+    public class AsignarPedidoCommand : IRequest<bool>
     {
-        public AsignarPedidoCommand(Pedido @event)
+        public AsignarPedidoCommand(Pedido pedidon)
         {
-            @event.numeroDePedido = new Random().Next();
+            this.pedido = pedidon;
+        }
+
+        public Pedido pedido { get; set; }
+    }
+    public class UpdatePedidoCommandHandler : IRequestHandler<AsignarPedidoCommand, bool>
+    {
+
+        private readonly ILogger<UpdatePedidoCommandHandler> _logger;
+
+        public UpdatePedidoCommandHandler(ILogger<UpdatePedidoCommandHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<bool> Handle(AsignarPedidoCommand request, CancellationToken cancellationToken)
+        {
+            request.pedido.numeroDePedido = new Random().Next();
+            return true;
         }
     }
 }
